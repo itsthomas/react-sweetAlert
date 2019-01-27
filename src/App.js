b5021d055f5c4cee48988e34c60955aa8d674ac2
Thomas <itsthomas@outlook.com>
2019-01-27 13:30:04 +0000

    simple dialog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SweetAlert from 'react-bootstrap-sweetalert';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    alert: null
+  };
+
+  showAlert() {
+    const getAlert = () => (
+      <SweetAlert
+        warning
+        showCancel
+        confirmBtnText="Yes!"
+        confirmBtnBsStyle="warning"
+        cancelBtnBsStyle="default"
+        title="Are you sure you want to delete this project?"
+        onConfirm={() => this.deleteFile()}
+        onCancel={() => this.cancelDelete()}
+      >
+        You will not be able to recover this project!
+      </SweetAlert>
+    );
+    this.setState({
+      alert: getAlert()
+    });
+  }
+
+  deleteFile() {
+    console.log('Do here whatever you need');
+    this.setState({
+      alert: null
+    });
+  }
+
+  cancelDelete() {
+    this.setState({
+      alert: null
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button
+          id="button__project-delete"
+          style={{ boxShadow: 'none', padding: '20px', margin: '20px' }}
+          type="submit"
+          onClick={() => this.showAlert()}
+        >
+          Delete
+        </button>
+        {this.state.alert}
       </div>
     );
   }
